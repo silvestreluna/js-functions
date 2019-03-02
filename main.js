380369682da948525047baed18f0dbeec6443be6
Silvestre Luna <slvtrluna@gmail.com>
2019-03-02 13:46:51 -0600

    adding something

--- main.js
+++ main.js
@@ -38,8 +38,24 @@ console.log(nuggetizer('pig'));
 // output = favorite dog breed is border collie
 
 const dogBreed = (dog) => {
-    return `My favorite dog breed is a ${dog}`;
+    return `My favorite dog breed is ${dog}`;
 };
 
 console.log(dogBreed('Border Collie'));
 console.log(dogBreed('Lab'));
+
+// const dogBreedDiv =  document.getElementById('dog-breed');
+// dogBreedDiv.innerHTML = dogBreed('lab');
+
+// const nugget = document.getElementById('nuggetizer');
+// nugget.innerHTML = nuggetizer('chicken');
+// nugget.innerHTML += nuggetizer('chicken');
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+};
+
+printToDom('dog-breed', dogBreed('lab'));
+printToDom('nuggetizer', nuggetizer('chicken'));
+printToDom('nuggetizer', 'Mmmmmmm');
